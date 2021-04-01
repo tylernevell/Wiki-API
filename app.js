ce557df0c15b2006f177a73bbbaabfc75604132a
Tyler Nevell <tylernevell@gmail.com>
2021-04-01 13:52:42 -0700

    Add Get One document fuctionality.

--- app.js
+++ app.js
@@ -58,6 +58,20 @@ app.route("/articles")
         });
     });
 
+app.route("/articles/:articleTitle")
+
+    .get((req, res) => {
+        Article.findOne({title: req.params.articleTitle}, (err, foundArticle) => {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else if (err) {
+                res.send(err);
+            } else {
+                console.log("No article found.");
+            }
+        });
+    });
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
